180b3facbfe3e419b8cdb1d4620b010edabdb8e3
lhc <marco.li@ximalaya.com>
2020-06-29 14:19:30 +0800

    AppBar渐变显示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
+const APPBAR_SCROLL_OFFSET = 100;
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -13,34 +15,75 @@ class _HomePageState extends State<HomePage> {
     'http://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg',
   ];
 
+  double _appBarAlpha = 0;
+
   @override
   Widget build(BuildContext context) {
-    return MediaQuery.removePadding(
-        removeTop: true,//移除顶部Status bar的Padding
-        context: context,
-        child: ListView(
-          children: <Widget>[
-            Container(
-              height: 160,
-              child: Swiper(
-                itemCount: _imgUrls.length,
-                autoplay: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.network(
-                    _imgUrls[index],
-                    fit: BoxFit.fill,
-                  );
-                },
-                pagination: SwiperPagination(),
+    return Stack(
+      children: <Widget>[
+        MediaQuery.removePadding(
+            removeTop: true, //移除顶部Status bar的Padding
+            context: context,
+            child: NotificationListener(
+              onNotification: (notification) {
+                if (notification is ScrollUpdateNotification &&
+                    notification.depth == 0) {
+                  _onScroll(notification.metrics.pixels);
+                }
+                return false;
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imgUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imgUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('占位'),
+                    ),
+                  ),
+                ],
+              ),
+            )),
+        Opacity(
+          opacity: _appBarAlpha,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(color: Colors.white),
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text('首页'),
               ),
             ),
-            Container(
-              height: 800,
-              child: ListTile(
-                title: Text('占位'),
-              ),
-            ),
-          ],
-        ));
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _onScroll(double offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      _appBarAlpha = alpha;
+    });
+    print('alpha:$alpha');
   }
 }
